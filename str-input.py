670a94d34f446769d8b9b028b4227fc77460a172
ammett <32433064+jesuispy@users.noreply.github.com>
2019-12-10 20:39:31 -0400

    Create str-input.py

---
+++ str-input.py
@@ -0,0 +1,6 @@
+name= input("What is your name?") #prompts user fro string input
+quest= input("What is your quest?")
+color= input("What is your favorite color?")
+
+print("So your name is "  + name +  " your quest is "  + quest +  " and your favorite color is "  + color +  ".") 
+#prints output as a string and included user input values
