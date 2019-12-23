ced8dd3e11783f46d9ea4f5781411a338fdfac70
Iam Serverless <iamserverless@gmail.com>
2019-12-23 16:37:08 -0400

    List Dictionaries Tuples
    Signed-off-by: Iam Serverless <iamserverless@gmail.com>

---
+++ Code  - 2  List  Dictonaries & Tuples.py
@@ -0,0 +1,28 @@
+store = ["some string", 123, "test"] #example list with intergers
+
+print(store)
+
+store.append("addition to list") #adding item to list
+print(store)
+
+store.pop()
+print(store) #remove last item on list
+
+print(len(store)) #displays lenght of var store
+
+print(store[0]) #display items in list count starts from 0
+
+#dictonary key and value
+
+diction = {"fruit": "banana",
+          "cereal": "oatmeal",
+          "drink": "water"}
+print("All values in the dictionary are", diction)
+print("Values under fruit are", diction['fruit'])
+diction["fruit"] = "apple" #update value in dictionary
+print("All values in the dictionary are", diction)
+
+#tuples are 2 value pairs
+tups = "item1", "item2", "item3", "item4"
+
+print(tups[3])
