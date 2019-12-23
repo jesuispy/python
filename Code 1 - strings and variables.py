3a4be7dd22c445cceed8705d14c43d0aac51100d
Iam Serverless <iamserverless@gmail.com>
2019-12-23 11:23:48 -0400

    V1
    Signed-off-by: Iam Serverless <iamserverless@gmail.com>

---
+++ Code 1 - strings and variables.py
@@ -0,0 +1,9 @@
+variable = "This is a variable string with numbers 123 " # variable with string and interger
+build = 10  #interger
+run = 5
+print(variable + "joined with another string")
+
+print(build, run)
+
+print(run+build) #simple math with variable
+
