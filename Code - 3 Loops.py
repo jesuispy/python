033256c1bfcdada8a8662922058cdc334a3f9a77
Iam Serverless <iamserverless@gmail.com>
2019-12-24 12:52:50 -0400

    Loops

---
+++ Code - 3 Loops.py
@@ -0,0 +1,26 @@
+for alphabet in 'You are awesome':     # traversal of a string sequence
+   print ('Current Letter :', alphabet)
+print()
+fruits = ['grapes', 'mango',  'water mellon']
+
+for musteat in fruits:        # traversal of List sequence
+   print ('Current fruit :', musteat)
+
+print("You are now healthy!")
+
+#iterations
+hand = [1, 2, 2, 2, 2, 2, 2, "mauby"]
+
+i = 0
+for item in hand:
+    i = i + 2
+    print(i)
+
+for item in hand:
+    if item == 2:
+        print("say it with me")
+ #While loop
+
+
+
+
