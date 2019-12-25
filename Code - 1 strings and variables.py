59da3b384884cf6cc7e3ef5fefc4b6999ed63252
Iam Serverless <iamserverless@gmail.com>
2019-12-25 14:52:37 -0400

    string and variables

--- Code - 1 strings and variables.py
+++ Code - 1 strings and variables.py
@@ -1,9 +1,11 @@
 variable = "This is a variable string with numbers 123 " # variable with string and interger
 build = 10  #interger
 run = 5
-print(variable + "joined with another string")
+print(variable + "joined with another string \'Yeah\'")
 
 print(build, run)
 
 print(run+build) #simple math with variable
+
+
 
