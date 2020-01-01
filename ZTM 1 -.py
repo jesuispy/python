8c9ab4f3ecd890bd294d4987e4d9dadee06d115f
Iam Serverless <iamserverless@gmail.com>
2020-01-01 13:55:17 -0400

    ZTM string input data type fundamental

--- ZTM 1 -.py
+++ ZTM 1 -.py
@@ -2,3 +2,16 @@ print('aw') #string
 
 yourname = input('what is my name ') #variable accepting values
 print('hello please to meet you', yourname) #printing string and input value
+
+
+#Fundamental Data types
+print(2+7)
+print(type(2+4))
+print(type(2-4))
+print(type(2*4))
+print(type(2/4)) #number with a decimal point float need more memory to store the number because there is no easy way to represent it in binary
+print(type(0))
+print(type(2/4.5)) #this is a float
+
+print (4 ** 10) #to the power
+print (10 % 9) #modulo
