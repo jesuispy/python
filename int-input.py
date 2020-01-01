5949c3c3cd350ac85b9fdced20a483b8495df4b3
Iam Serverless <iamserverless@gmail.com>
2020-01-01 09:30:56 -0400

    Create int-input.py

--- int-input.py
+++ int-input.py
@@ -1,3 +1,3 @@
 inthere = int(input("Enter a number")) #accepts intergers  as the value input
 
-print(inthere + 10)  #prints output and add 10 to the value of the user inputted intergers test 
+print(inthere + 10)  #prints output and add 10 to the value of the user inputted interger
