7c9165190a3b7880a6396390b6f70e4aa3783f15
Iam Serverless <iamserverless@gmail.com>
2020-01-02 18:05:44 -0400

    ZTM string input data type fundamental

--- ZTM 2.py
+++ ZTM 2.py
@@ -6,3 +6,20 @@ print("say what",money)
 money += 2 #augmented assignment operator used
 print("You sure",money)
 
+first_name = input("what is you first name ")
+print(type(first_name))
+
+name1 = "test "
+name2 = "me"
+wholename = name1 + name2
+print(wholename)
+print(name1 + name2) #string concatenation
+print(str(10000)) #string type
+print(type(str(10000)))
+print(int(100))
+print(type(int(100)))
+x = str(10) #long way
+y = int(x)
+z = type(y)
+print(z)
+
