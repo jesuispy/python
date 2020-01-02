0b996bce4d17e8ea79755b51df3e9c63c3ee6526
Iam Serverless <iamserverless@gmail.com>
2020-01-02 19:01:31 -0400

    ZTM string input data type fundamental

--- ZTM 2.py
+++ ZTM 2.py
@@ -23,3 +23,22 @@ y = int(x)
 z = type(y)
 print(z)
 
+
+actor = 'Malcom'
+age = 20
+print(f'Hi {actor}. You are {age} years old')
+print(len(actor)) #len is lenght count start from 1 not 0
+#method
+alphabeth = 'i will code soon'
+print(alphabeth.upper())
+print(alphabeth.capitalize())
+print(alphabeth.find("will"),"is the starting point")
+
+print(alphabeth.replace("will", "learnt")) #replace
+
+birth_year = int(input("What year were you born "))
+age = 2019 - birth_year
+print(f'your age us: {age}')
+
+
+
