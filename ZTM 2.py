74fc1bbd512d89eea052f2788f425e0513509514
Iam Serverless <iamserverless@gmail.com>
2020-01-02 17:17:30 -0400

    ZTM string input data type fundamental

--- ZTM 2.py
+++ ZTM 2.py
@@ -1,1 +1,8 @@
 
+money = 100
+print("how much money is it", money)
+money -= 2 #augmented assignment operator used
+print("say what",money)
+money += 2 #augmented assignment operator used
+print("You sure",money)
+
