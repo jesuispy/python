556624617dcd0d9976120de6d98fc45e23168443
Iam Serverless <iamserverless@gmail.com>
2020-01-08 19:04:16 -0400

    ZTM string input data type fundamental

--- ZTM 3.py
+++ ZTM 3.py
@@ -1,9 +1,21 @@
 
 username = input('what is you name here ')
-ids = len(input('what is you password '))
+ ids = len(input('what is you password '))
 lenght = ('*' * ids)
 
 print(f'{username}, your password {lenght} is {ids} letters long')
 
 #list
 
+cart = ["you at cool",
+        "you are awesome",
+        "you are a waterboy",
+        "milk is good"]
+print (cart [0])
+print (cart[0::2])
+
+cart[0] = 'ice water'
+cart_1 = cart[:]
+cart_1 [1] = 'chicken'
+print(cart)
+print(cart_1)
