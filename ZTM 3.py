28349d1b2f4163af771ce435d4e738683f3194de
Iam Serverless <iamserverless@gmail.com>
2020-01-08 18:03:51 -0400

    list

--- ZTM 3.py
+++ ZTM 3.py
@@ -1,1 +1,9 @@
 
+username = input('what is you name here ')
+ids = len(input('what is you password '))
+lenght = ('*' * ids)
+
+print(f'{username}, your password {lenght} is {ids} letters long')
+
+#list
+
