85a0d4f9dea3c5c50081790c7ace729eb22801d8
ammett <32433064+jesuispy@users.noreply.github.com>
2020-02-18 19:21:04 -0400

    Update stringsgc.py

--- stringsgc.py
+++ stringsgc.py
@@ -22,3 +22,11 @@ def exam_score(name, grade):
 print(exam_score("Reed", 80))
 print(exam_score("Paige", 92))
 print(exam_score("Jesse", 85))
+
+def group_list(group, users):
+  members = group + (str(": ")) + ",".join(users)
+  return members
+
+print(group_list("Marketing", ["Mike", "Karen", "Jake", "Tasha"])) # Should be "Marketing: Mike, Karen, Jake, Tasha"
+print(group_list("Engineering", ["Kim", "Jay", "Tom"])) # Should be "Engineering: Kim, Jay, Tom"
+print(group_list("Users", "")) # Should be "Users:"
