c36d3676afbeebaddbcf2e8e1d47a96e9e3115f3
jesuispy <32433064+jesuispy@users.noreply.github.com>
2020-05-06 06:24:47 -0400

    test to remote

--- datatypes/usersaidinp.py
+++ datatypes/usersaidinp.py
@@ -3,7 +3,5 @@ print(user_said)
 
 number_only = int(input("Type a number between 1 -10 "))
 
-
-
 print("The number you selected is", str(number_only))
 print("The number you selected is", number_only)
