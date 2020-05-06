b26e1b2401820bb961878b2628a205136174f9d6
jesuispy <32433064+jesuispy@users.noreply.github.com>
2020-05-06 06:23:24 -0400

    Input options

--- datatypes/usersaidinp.py
+++ datatypes/usersaidinp.py
@@ -1,4 +1,9 @@
-user_said= input("Tell me about you ")
-
+user_said = input("Tell me about you ")
 print(user_said)
 
+number_only = int(input("Type a number between 1 -10 "))
+
+
+
+print("The number you selected is", str(number_only))
+print("The number you selected is", number_only)
